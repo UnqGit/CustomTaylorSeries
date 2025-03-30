dea09ce6c8fc692b1592c75494b338dad4dcdda2
Vishesh Meena <visheshmeena47@gmail.com>
2025-03-30 19:49:37 +0530

    finalised

--- taylor.cpp
+++ taylor.cpp
@@ -5,54 +5,111 @@
 #include <algorithm>
 #include <sstream>
 #include <functional>
+#include <regex>
 #include <cmath>
 
 void start_text(){
-    std::cout << "In the program you have to enter\nthe coefficients from the highest coefficient\nfor eg, 2 0 0 would be 2x^2 and 1 0 would be x\n";
-    std::cout << "The available functions will be\nsqrt, exp, log, tan, sin, cos, atan, acos, asin\n\n";
+    std::cout << "In the program you can enter coefficients for in multiple ways:\n";
+    std::cout << "\t1.) INPUT: 3 2 0 1; the function would be 3x³ + 2x² + 0x¹ + 1.\n";
+    std::cout << "\t2.) INPUT: 3,2,0,1; function would be same as above(i.e, highest degree first).\n";
+    std::cout << "\t3.) INPUT: 3x^3 + 2x^2 + 1; these terms can be in any order(power denoted via '^').\n";
+    std::cout << "\t4.) INPUT: 3x3+2x2+1; this is also valid(number following x is regarded as power).\n";
+    std::cout << "\t4.) INPUT: 3x³+2x²+1; this is also valid(superscipt as power).\n";
+    std::cout << "\t5.) INPUT: 3x^3 2x^2 1; this is also valid.\n";
+    std::cout << "\t6.) INPUT: 3X³ 2X^2 + x1 + 1; this is also valid.\n\n";
+    std::cout << "NOTE: things like:\n";
+    std::cout << "\t1.) 2² are not valid and would be considered as 22.\n";
+    std::cout << "\t2.) Parenthesis(\"()\") are not allowed.\n";
+    std::cout << "\t3.) x²x is invalid.\n";
+    std::cout << "\t4.) x-+x² is also invalid.\n\n";
+    std::cout << "The available functions will be\nsqrt, exp, log, tan, sin, cos, atan, acos, asin, sinh, cosh, tanh\n\n";
 }
 
 void input_corrector(std::string& input){
-    std::replace(input.begin(), input.end(), ',', ' ');
+    input = std::regex_replace(input, std::regex(","), " ");
     input.erase(0, input.find_first_not_of(" 0\n\t"));
     input.erase(input.find_last_not_of(" \n\t")+1);
+    input = std::regex_replace(input, std::regex("\t"), " ");
+    input = std::regex_replace(input, std::regex("\n"), " ");
     if(input.empty()) input = "0";
     for(char& ch : input) ch = std::tolower(ch);
 }
 
-bool invalid_input(std::string input){
-    if(input.empty()) return 1;
+bool invalid_input(std::string& input){
+    if(input.find('#')!=std::string::npos) return 1;
+    std::string super[] = {"⁰", "¹", "²", "³", "⁴", "⁵", "⁶", "⁷", "⁸", "⁹"};
+    for(int i = 0; i <= 9; i++){
+        input = std::regex_replace(input, std::regex(super[i]), std::to_string(i));
+        std::string temp = "x";
+        temp+=std::to_string(i);
+        std::string todo="x^"+std::to_string(i);
+        input = std::regex_replace(input, std::regex(temp), todo);
+    }
+    if(input.find("x^-")!=std::string::npos) return true;
+    input = std::regex_replace(input, std::regex("x\\^\\+"), "x^");
+    input = std::regex_replace(input, std::regex("x\\^"), "#");
+    input = std::regex_replace(input, std::regex("x"), "#1");
+    input = std::regex_replace(input, std::regex("\\-"), " -");
+    input = std::regex_replace(input, std::regex("\\+"), " +");
+    input = std::regex_replace(input, std::regex(" #"), " 1#");
+    input = std::regex_replace(input, std::regex("\\-#"), "-1#");
+    input = std::regex_replace(input, std::regex("\\+#"), "+1#");
+    while(input.find("  ")!=std::string::npos){
+        input = std::regex_replace(input, std::regex("  "), " ");
+    }
+    input = std::regex_replace(input, std::regex("# "), "#");
+    input = std::regex_replace(input, std::regex("\\- "), "-");
+    input = std::regex_replace(input, std::regex("\\+ "), "+");
+    if(input.find("#-")!=std::string::npos)return true;
+    if(input[0]=='#') input.insert(input.begin(), '1');
     for(int i = 0; i < input.size(); i++){
         char j = input[i];
         if(!(std::isdigit(j))){
             if(j=='-'||j=='+'){
-                if(i + 1 < input.size()){
+                if(i > 0){
+                    if(std::isdigit(input[i-1])){
+                        input.insert(input.begin()+i, ' ');
+                        i++;
+                    }
+                }
+                if(i + 1 < input.length()){
                     if(std::isdigit(input[i + 1])) continue;
                     else if(input[i + 1] == '.'){
                         if(i + 2 < input.size()){
                             if(std::isdigit(input[i + 2])) continue;
-                            return 1;
+                            return true;
                         }
                     }
                 }
-                else return 1;
+                else return true;
             }
             else if(j=='.'){
                 if(i > 0){
                     char k = input[i-1];
-                    if(std::isdigit(k) || k == '-' || k == '+') continue;
+                    if(!(std::isdigit(k) || k == '-' || k == '+' || k == ' ')) return true;
                 }
                 if(i + 1 < input.size()){
                     char k = input[i + 1];
-                    if(std::isdigit(k)) continue;
+                    if(!(std::isdigit(k))) return true;
                 }
-                return 1;
+                continue;
             }
-            else if(j==' ' || j=='\n' || j=='\t') continue;
-            return 1;
+            else if(j=='#' || j==' ') continue;
+            return true;
+        }
+        else{
+            if(i > 0){
+                if(input[i - 1]=='#'){
+                    while(i+1<input.length()){
+                        if(input[i+1]==' ')break;
+                        if(!std::isdigit(input[i+1])) return true;
+                        i++;
+                    }
+                }
+            }
         }
     }
-    return 0;
+    return false;
 }
 
 bool cond(const std::string& part,const std::string& str){
@@ -65,8 +122,8 @@ bool function_checker(std::string& func){
     if(cond("as", func)){func = "asin"; return 0;}
     if(cond("ac", func)){func = "acos"; return 0;}
     if(cond("sq", func)){func = "sqrt"; return 0;}
-    if(cond("nh", func)){func = "tanh"; return 0;}
     if(cond("inh", func)){func = "sinh"; return 0;}
+    if(cond("nh", func)){func = "tanh"; return 0;}
     if(cond("sh", func)){func = "cosh"; return 0;}
     if(cond("ta", func)){func = "tan"; return 0;}
     if(cond("si", func)){func = "sin"; return 0;}
@@ -77,7 +134,7 @@ bool function_checker(std::string& func){
 
 long double derivatives(const std::function<long double(long double)>& toBeDoneOn, long double a, int i){
     if(i == 0) return toBeDoneOn(a);
-    long double h = (i<=4)?1.0/(1<<10):1.0/(1<<8);
+    long double h = (i<=4)?1.0/(1<<10):(i<12)?1.0/(1<<8):1.0/(1<<5);
     auto f = [&](long double d){
         return (derivatives(toBeDoneOn, a+h*d, i-1)-derivatives(toBeDoneOn, a-h*d, i-1))/(2*h*d);
     };
@@ -176,29 +233,64 @@ std::string getInput(const std::string& prompt,const bool& funcornums){
         input_corrector(input);
         invalid = funcornums?function_checker(input):invalid_input(input);
         if(!invalid) break;
-        std::cout << "Invalid " << (funcornums?"function":"numbers") << ".\nEnter again:\n";
+        std::cout << "Invalid " << (funcornums?"function":"polynomial/coefficients") << ".\nEnter again:\n";
     }while(true);
     std::cin.clear();
     return input;
 }
 
+std::vector<long double> get_vec(const std::string& input){
+    std::vector<long double> coefs;
+    std::vector<int> powers;
+    std::vector<std::string> separation;
+    std::stringstream ss(input);
+    std::string temp, temp2;
+    int power;
+    while(ss >> temp) separation.push_back(temp);
+    if(input.find('#')!=std::string::npos){
+        for(int i = 0; i < separation.size(); i++){
+            temp = separation[i];
+            if(temp.find('#')!=std::string::npos){
+                int z = temp.find('#');
+                coefs.push_back(std::stold(temp.substr(0, z)));
+                powers.push_back(std::stoi(temp.substr(z + 1, temp.size()-z)));
+            }
+            else{
+                coefs.push_back(std::stold(temp));
+                powers.push_back(0);
+            }
+        }
+        power = *max_element(powers.begin(), powers.end());
+        std::vector<long double> result(power+1, 0);
+        for(int i = 0; i < powers.size(); i++){
+            int it = powers[i];
+            result[power - it]+=coefs[i];
+        }
+        return result;
+    }
+    else{
+        std::vector<long double> result;
+        for(int i = 0; i < separation.size(); i++){
+            result.push_back(std::stold(separation[i]));
+        }
+        return result;
+    }
+}
+
 std::tuple<std::vector<long double>, std::vector<long double>> get_coef(std::string& func){
     std::vector<long double> polycoef, polycoef2;
     std::string prompt, input;
-    long double coef, coef2;
-    prompt = "Enter the coefficients of";
+    prompt = "Enter the";
     prompt+=(func=="pow"?" base":func=="log"?" function":"");
-    prompt+=" polynomial:\n";
+    prompt+=" polynomial/coefficients:\n";
     input = getInput(prompt, false);
-    std::stringstream ss(input);      
-    while(ss >> coef) polycoef.push_back(coef);
+    polycoef = get_vec(input);
     if(func=="pow"||func=="log"){
-        prompt = "Enter the coefficients of ";
+        prompt = "Enter the ";
         prompt+=(func=="pow"?"exponent":"base(leave empty or enter 0 to get natural log)");
-        prompt+=" polynomial:\n";
+        prompt+=" polynomial/coefficients:\n";
         input = getInput(prompt, false);
-        std::stringstream sd(input);
-        while(sd >> coef2) polycoef2.push_back(coef2);
+        polycoef2 = get_vec(input);
     }
     return std::tuple<std::vector<long double>, std::vector<long double>>(polycoef, polycoef2);
 }
